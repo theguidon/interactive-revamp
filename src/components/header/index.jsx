c6a4c4699ce0f8c9c3a6b6f2c0d720b2941f9817
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-22 22:14:44 +0800

    add links to header, add fonts

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
+
+import logo from "./../../assets/images/logos/interactive-longform.svg";
 
 function Header() {
-  return <header>HEADER</header>;
+  return <header>
+    <div className="general-container">
+      <Link to="/">
+        <img
+          src={logo}
+        />
+      </Link>
+
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
+  </header>;
 }
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./assets/css/global.css";
 import { BrowserRouter } from "react-router-dom";
+
+import "./assets/css/global.css";
+import "./assets/css/fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
