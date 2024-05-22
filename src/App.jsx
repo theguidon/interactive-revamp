c3e9e6830df2b3e27d30b00e7394432788c8f122
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-22 17:21:11 +0800

    Add boilerplate for Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Carousel from "./components/Carousel";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Carousel />
+    </>
+  );
 }
 
 export default App;
