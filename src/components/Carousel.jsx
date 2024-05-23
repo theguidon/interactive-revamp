803be75dd39f633c24f2e7ed6a83267d123d54c8
Arthur Lluisma <ajclluisma@gmail.com>
2024-05-23 14:23:55 +0800

    Add complete mobile design for carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import exitIcon from "../assets/images/ExitModalIcon.png";
 import "../styles/Carousel.css";
 
 const json = await fetch("/data.json").then((r) => r.json());
 
 const Carousel = () => {
   const [current, setCurrent] = useState(0);
+  const [modal, setModal] = useState(false);
 
   const changeArticle = () => {
     setCurrent(current === 2 ? 0 : current + 1);
   };
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("carousel__activeModal");
+  } else {
+    document.body.classList.remove("carousel__activeModal");
+  }
 
   return (
     <header className="carousel">
@@ -34,10 +46,7 @@ const Carousel = () => {
                   <p>{carousel.title}</p>
                   <p>{carousel.description}</p>
                   <section className="carousel__articleActive__staffs">
-                    <section
-                      className="carousel__staffs__staff"
-                      id="carousel__staff__writers"
-                    >
+                    <section className="carousel__staffs__staff">
                       <p
                         className="carousel__staffNames"
                         style={
@@ -64,10 +73,7 @@ const Carousel = () => {
                         </span>
                       </div>
                     </section>
-                    <section
-                      className="carousel__staffs__staff"
-                      id="carousel__staff__photos"
-                    >
+                    <section className="carousel__staffs__staff">
                       <p
                         className="carousel__staffNames"
                         style={
@@ -94,10 +100,7 @@ const Carousel = () => {
                         </span>
                       </div>
                     </section>
-                    <section
-                      className="carousel__staffs__staff"
-                      id="carousel__staff__videos"
-                    >
+                    <section className="carousel__staffs__staff">
                       <p
                         className="carousel__staffNames"
                         style={
@@ -124,10 +127,7 @@ const Carousel = () => {
                         </span>
                       </div>
                     </section>
-                    <section
-                      className="carousel__staffs__staff"
-                      id="carousel__staff__illustrations"
-                    >
+                    <section className="carousel__staffs__staff">
                       <p
                         className="carousel__staffNames"
                         style={
@@ -154,10 +154,7 @@ const Carousel = () => {
                         </span>
                       </div>
                     </section>
-                    <section
-                      className="carousel__staffs__staff"
-                      id="carousel__staff__interactives"
-                    >
+                    <section className="carousel__staffs__staff">
                       <p
                         className="carousel__staffNames"
                         style={
@@ -197,9 +194,138 @@ const Carousel = () => {
                       href="#"
                       className="carousel__button"
                       id="carousel__button--credits"
+                      onClick={toggleModal}
                     >
                       Show credits
                     </a>
+                    {modal && (
+                      <section className="carousel__modal">
+                        <div onClick={toggleModal} className="overlay"></div>
+                        <section className="carousel__modalContent">
+                          <h2>{carousel.title}</h2>
+                          <section className="carousel__staffs--modal">
+                            <section className="carousel__staff--modal">
+                              <p
+                                className="carousel__staffNames--modal"
+                                style={
+                                  carousel.writers_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                Written by
+                              </p>
+                              <p
+                                className="carousel__bylines--modal"
+                                style={
+                                  carousel.writers_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.writers_byline}
+                              </p>
+                            </section>
+                            <section className="carousel__staff--modal">
+                              <p
+                                className="carousel__staffNames--modal"
+                                style={
+                                  carousel.photos_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                Photos by
+                              </p>
+                              <p
+                                className="carousel__bylines--modal"
+                                style={
+                                  carousel.photos_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.photos_byline}
+                              </p>
+                            </section>
+                            <section className="carousel__staff--modal">
+                              <p
+                                className="carousel__staffNames--modal"
+                                style={
+                                  carousel.videos_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.videos_byline != ""
+                                  ? "Videos by"
+                                  : ""}
+                              </p>
+                              <p
+                                className="carousel__bylines--modal"
+                                style={
+                                  carousel.videos_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.videos_byline}
+                              </p>
+                            </section>
+                            <section className="carousel__staff--modal">
+                              <p
+                                className="carousel__staffNames--modal"
+                                style={
+                                  carousel.graphics_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                Illustrations by
+                              </p>
+                              <p
+                                className="carousel__bylines--modal"
+                                style={
+                                  carousel.graphics_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.graphics_byline}
+                              </p>
+                            </section>
+                            <section className="carousel__staff--modal">
+                              <p
+                                className="carousel__staffNames--modal"
+                                style={
+                                  carousel.interactive_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                Interactive by
+                              </p>
+                              <p
+                                className="carousel__bylines--modal"
+                                style={
+                                  carousel.interactive_byline != ""
+                                    ? { margin: "0.2rem" }
+                                    : { margin: "0" }
+                                }
+                              >
+                                {carousel.interactive_byline}
+                              </p>
+                            </section>
+                          </section>
+                          <button
+                            className="carousel__closeModal"
+                            onClick={toggleModal}
+                          >
+                            <img src={exitIcon} alt="Exit Modal" />
+                          </button>
+                        </section>
+                      </section>
+                    )}
                   </section>
                 </section>
               </>
