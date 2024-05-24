4aae0c76a776f6200c4a512e9dbd4d430745d36e
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-25 02:01:36 +0800

    first iteration for article-card

--- src/components/article-card/index.jsx
+++ src/components/article-card/index.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 function ArticleCard(props) {
-  return <>ArticleCard</>;
+  const [active, setActive] = useState(false);
+
+  return (
+    <div
+      className={`article-card ${active ? "active" : ""}`}
+      onClick={() => setActive((prev) => !prev)}
+    >
+      <div className="cover-container">
+        <img className="cover" src={props.article.cover} />
+        <svg
+          className="polygon"
+          viewBox="0 0 23 22"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14.6162 19.1454C13.3163 21.6873 9.68368 21.6873 8.3838 19.1454L1.49854 5.68093C0.307629 3.35206 1.99903 0.587402 4.61473 0.587402L18.3853 0.587402C21.001 0.587402 22.6924 3.35206 21.5015 5.68093L14.6162 19.1454Z"
+            fill="#53BC91"
+            stroke="#53BC91"
+          />
+        </svg>
+      </div>
+
+      <p className="title">{props.article.title}</p>
+    </div>
+  );
 }
 
 export default ArticleCard;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,13 +3,21 @@ import React from "react";
 import "./index.css";
 import FocusCard from "../../components/focus-card";
 import CarouselHero from "../../components/carousel-hero";
+import ArticleCard from "../../components/article-card";
+import { useSelector } from "react-redux";
 
 function MainPage() {
+  const articles = useSelector((state) => state.articles);
+
   return (
     <div id="home">
       <CarouselHero />
 
       <main className="general-container">
+        {!articles.isLoading && !articles.isError && (
+          <ArticleCard article={articles.data[0]} />
+        )}
+
         <FocusCard />
       </main>
     </div>
